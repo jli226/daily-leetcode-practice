c34617f940e7fe6aeb9acbe69d39ff1debd441ab
Jenny Li <jli226@fordham.edu>
2020-04-20 21:41:59 -0700

    ThirdMaximumNumber

---
+++ certainTypeQuestions/Array/414.ThirdMaximumNumber.py
@@ -0,0 +1,22 @@
+# Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+# Example 1:
+# Input: [3, 2, 1]
+
+# Output: 1
+
+# Explanation: The third maximum is 1.
+
+
+class Solution(object):
+    def thirdMax(self, nums):
+        v = [float('-inf'), float('-inf'), float('-inf')]
+        for num in nums:
+            if num not in v:
+                if num > v[0]:
+                    v = [num, v[0], v[1]]
+                elif num > v[1]:
+                    v = [v[0], num, v[1]]
+                elif num > v[2]:
+                    v = [v[0], v[1], num]
+        return max(nums) if float('-inf') in v else v[2]
