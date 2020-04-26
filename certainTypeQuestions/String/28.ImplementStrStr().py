9707162423f17f09e2c4632ec3f25995b5262d2d
Jenny Li <jli226@fordham.edu>
2020-04-25 23:40:43 -0700

    answer28

--- certainTypeQuestions/String/28.ImplementStrStr().py
+++ certainTypeQuestions/String/28.ImplementStrStr().py
@@ -25,3 +25,16 @@
 # What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 # For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
