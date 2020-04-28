41f1ab7ca5ff1b14a82a3da9908b7177de6ef278
Jenny Li <jli226@fordham.edu>
2020-04-27 22:19:52 -0700

    a387

--- certainTypeQuestions/String/387.FirstUniqueCharacterinaString.py
+++ certainTypeQuestions/String/387.FirstUniqueCharacterinaString.py
@@ -8,3 +8,14 @@
 # s = "loveleetcode",
 # return 2.
 # Note: You may assume the string contain only lowercase letters.
+
+
+def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        index = [s.index(l) for l in letters if s.count(l) == 1]
+        return min(index) if len(index) > 0 else -1
