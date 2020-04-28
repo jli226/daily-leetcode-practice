f550a73d93a82aaec066d4d3ab2cb097b44bdeb9
Jenny Li <jli226@fordham.edu>
2020-04-27 22:21:50 -0700

    a443

--- certainTypeQuestions/String/443.StringCompression.py
+++ certainTypeQuestions/String/443.StringCompression.py
@@ -22,3 +22,18 @@
 
 # Explanation:
 # "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+
+class Solution(object):
+    def compress(self, chars):
+        left = i = 0
+        while i < len(chars):
+            char, length = chars[i], 1
+            while (i + 1) < len(chars) and char == chars[i + 1]:
+                length, i = length + 1, i + 1
+            chars[left] = char
+            if length > 1:
+                len_str = str(length)
+                chars[left + 1:left + 1 + len(len_str)] = len_str
+                left += len(len_str)
+            left, i = left + 1, i + 1
+        return left
