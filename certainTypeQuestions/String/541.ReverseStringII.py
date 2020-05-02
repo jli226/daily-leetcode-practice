83cc8ac00d960a789feae124153052bd7939bc30
Jenny Li <jli226@fordham.edu>
2020-05-01 23:18:09 -0700

    q551

--- certainTypeQuestions/String/541.ReverseStringII.py
+++ certainTypeQuestions/String/541.ReverseStringII.py
@@ -21,6 +21,6 @@
 
 def reverseStr(self, s, k):
     s = list(s)
-    for i in xrange(0, len(s), 2*k):
+    for i in range(0, len(s), 2*k):
         s[i:i+k] = reversed(s[i:i+k])
     return "".join(s)
