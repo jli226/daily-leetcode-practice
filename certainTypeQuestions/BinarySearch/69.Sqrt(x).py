c2b982569ef9918aa9ea51c3f1bdd537adb4f5b3
Jenny Li <jli226@fordham.edu>
2020-05-04 23:26:44 -0700

    a69

--- certainTypeQuestions/BinarySearch/69.Sqrt(x).py
+++ certainTypeQuestions/BinarySearch/69.Sqrt(x).py
@@ -24,3 +24,22 @@
 # # Output: 2
 # # Explanation: The square root of 8 is 2.82842..., and since
 #             the decimal part is truncated, 2 is returned.
+
+
+def mySqrt(self, x):
+        lo, hi = 0, x
+
+        while lo <= hi:
+            mid = (lo + hi) // 2
+
+            if mid * mid > x:
+                hi = mid - 1
+            elif mid * mid < x:
+                lo = mid + 1
+            else:
+                return mid
+
+        # When there is no perfect square, hi is the the value on the left
+        # of where it would have been (rounding down). If we were rounding up,
+        # we would return lo
+        return hi
