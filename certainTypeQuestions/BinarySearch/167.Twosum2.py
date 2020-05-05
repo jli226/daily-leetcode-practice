247988a9ca0461da9c4e9ec26d50842d9220301a
Jenny Li <jli226@fordham.edu>
2020-05-04 23:30:15 -0700

    a167

--- certainTypeQuestions/BinarySearch/167.Twosum2.py
+++ certainTypeQuestions/BinarySearch/167.Twosum2.py
@@ -21,3 +21,15 @@
 # Input: numbers = [2,7,11,15], target = 9
 # Output: [1,2]
 # Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+vector < int > twoSum(vector < int > & numbers, int target) {
+    int lo = 0, hi = numbers.size()-1
+    while (numbers[lo]+numbers[hi] != target){
+        if (numbers[lo]+numbers[hi] < target){
+            lo++
+        } else {
+            hi--
+        }
+    }
+    return vector < int > ({lo+1, hi+1})
+}
