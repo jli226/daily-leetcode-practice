460716ccd62dd962d6725e079484297cf23c2f8d
Jenny Li <jli226@fordham.edu>
2020-05-05 23:08:32 -0700

    a349

--- certainTypeQuestions/BinarySearch/349.IntersectionOfTwoArrays.py
+++ certainTypeQuestions/BinarySearch/349.IntersectionOfTwoArrays.py
@@ -22,3 +22,15 @@
 
 # Each element in the result must be unique.
 # The result can be in any order.
+
+
+class Solution(object):
+
+
+def intersection(self, nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    return list(set(nums1) & set(nums2))
