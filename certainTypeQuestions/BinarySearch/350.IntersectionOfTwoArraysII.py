752d050179cc044c3e6bf613fc57f47b3ffcd7d8
Jenny Li <jli226@fordham.edu>
2020-05-05 23:11:59 -0700

    a350

--- certainTypeQuestions/BinarySearch/350.IntersectionOfTwoArraysII.py
+++ certainTypeQuestions/BinarySearch/350.IntersectionOfTwoArraysII.py
@@ -27,3 +27,20 @@
 # What if the given array is already sorted? How would you optimize your algorithm?
 # What if nums1's size is small compared to nums2's size? Which algorithm is better?
 # What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+
+        counts = {}
+        res = []
+
+        for num in nums1:
+            counts[num] = counts.get(num, 0) + 1
+
+        for num in nums2:
+            if num in counts and counts[num] > 0:
+                res.append(num)
+                counts[num] -= 1
+
+        return res
