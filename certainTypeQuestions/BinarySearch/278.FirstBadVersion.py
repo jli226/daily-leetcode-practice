39a0a956a65b213a7df9eac7a02f6d653d09fa30
Jenny Li <jli226@fordham.edu>
2020-05-05 23:05:17 -0700

    a278

--- certainTypeQuestions/BinarySearch/278.FirstBadVersion.py
+++ certainTypeQuestions/BinarySearch/278.FirstBadVersion.py
@@ -23,3 +23,22 @@
 # call isBadVersion(4) -> true
 
 # Then 4 is the first bad version.
+
+
+class Solution(object):
+
+
+def firstBadVersion(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    r = n-1
+    l = 0
+    while(l <= r):
+        mid = l + (r-l)/2
+        if isBadVersion(mid) == False:
+            l = mid+1
+        else:
+            r = mid-1
+    return l
