8573fd20f846dfd5343d6e9817e778aad24792d4
Jenny Li <jli226@fordham.edu>
2020-05-07 22:07:44 -0700

    a100

--- certainTypeQuestions/Tree/100.SameTree.py
+++ certainTypeQuestions/Tree/100.SameTree.py
@@ -20,3 +20,40 @@
 #         [1,2],     [1,null,2]
 
 # Output: false
+
+# Stack
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        stack = [(p, q)]
+        while stack:
+            n1, n2 = stack.pop()
+            if not n1 and not n2:
+                continue
+            if not n1 or not n2:
+                return n1 == n2
+            if n1.val != n2.val:
+                return False
+            stack.append((n1.right, n2.right))
+            stack.append((n1.left, n2.left))
+        return True
+
+
+# Queue
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        queue = collections.deque([(p, q)])
+        while queue:
+            n1, n2 = queue.popleft()
+            if not n1 and not n2:
+                continue
+            if not n1 or not n2:
+                return n1 == n2
+            if n1.val != n2.val:
+                return False
+            queue.append((n1.left, n2.left))
+            queue.append((n1.right, n2.right))
+        return True
