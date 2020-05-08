4bf147407ab9b5f77ed6f06384d1579043857bbb
Jenny Li <jli226@fordham.edu>
2020-05-07 22:20:00 -0700

    a101

--- certainTypeQuestions/Tree/101.SymmetricTree.py
+++ certainTypeQuestions/Tree/101.SymmetricTree.py
@@ -21,3 +21,38 @@
 # Follow up: Solve it both recursively and iteratively.
 
 # Recursive solution:
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.equals(root.left, root.right)
+
+    def equals(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        elif node1 and node2 and node1.val == node2.val:
+            return self.equals(node1.left, node2.right) and self.equals(node1.right, node2.left)
+        else:
+            return False
+
+
+# Non-Recursive solution :
+
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        stack = []
+        stack.append((root.left, root.right))
+
+        while stack:
+            left, right = stack.pop()
+            if not left and not right:
+                continue
+            elif left and right and left.val == right.val:
+                stack.append((left.left, right.right))
+                stack.append((left.right, right.left))
+            else:
+                return False
+
+        return True
