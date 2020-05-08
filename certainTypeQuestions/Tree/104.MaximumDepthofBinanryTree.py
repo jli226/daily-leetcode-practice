e1e627cea14483f392d2d18ed7cb7754d2d21d73
Jenny Li <jli226@fordham.edu>
2020-05-07 22:26:02 -0700

    a104

--- certainTypeQuestions/Tree/104.MaximumDepthofBinanryTree.py
+++ certainTypeQuestions/Tree/104.MaximumDepthofBinanryTree.py
@@ -14,3 +14,56 @@
 #     /  \
 #    15   7
 # return its depth = 3.
+
+# recursion
+
+
+def maxDepth(self, root):
+
+
+if not root:
+return 0
+
+return 1+max(self.maxDepth(root.left), self.maxDepth(root.right))
+Always better to write a similar iterative solution:
+
+    # stack for level order
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        tstack, h = [root], 0
+
+        # count number of levels
+        while tstack:
+            nextlevel = []
+            while tstack:
+                top = tstack.pop()
+                if top.left:
+                    nextlevel.append(top.left)
+                if top.right:
+                    nextlevel.append(top.right)
+            tstack = nextlevel
+            h += 1
+        return h
+
+# queue for level order
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+
+        tqueue, h = collections.deque(), 0
+        tqueue.append(root)
+        while tqueue:
+            nextlevel = collections.deque()
+            while tqueue:
+                front = tqueue.popleft()
+                if front.left:
+                    nextlevel.append(front.left)
+                if front.right:
+                    nextlevel.append(front.right)
+            tqueue = nextlevel
+            h += 1
+        return h
