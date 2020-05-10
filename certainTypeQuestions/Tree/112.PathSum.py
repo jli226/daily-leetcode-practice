7df9031a64d4eb2bfd862f771b4feb245d1e81fb
Jenny Li <jli226@fordham.edu>
2020-05-09 22:53:14 -0700

    a112

--- certainTypeQuestions/Tree/112.PathSum.py
+++ certainTypeQuestions/Tree/112.PathSum.py
@@ -14,3 +14,20 @@
 #  /  \      \
 # 7    2      1
 # return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    # 1:27
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
