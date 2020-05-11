27141c1292c61bfdd84edbbafe2a99f21b2e6b70
Jenny Li <jli226@fordham.edu>
2020-05-11 10:35:47 -0700

    a21

--- certainTypeQuestions/LinkedList/21.MergerTwoSortedLists.py
+++ certainTypeQuestions/LinkedList/21.MergerTwoSortedLists.py
@@ -4,3 +4,24 @@
 
 # Input: 1->2->4, 1->3->4
 # Output: 1->1->2->3->4->4
+
+# in-place, iteratively
+
+
+def mergeTwoLists(self, l1, l2):
+    if None in (l1, l2):
+        return l1 or l2
+    dummy = cur = ListNode(0)
+    dummy.next = l1
+    while l1 and l2:
+        if l1.val < l2.val:
+            l1 = l1.next
+        else:
+            nxt = cur.next
+            cur.next = l2
+            tmp = l2.next
+            l2.next = nxt
+            l2 = tmp
+        cur = cur.next
+    cur.next = l1 or l2
+    return dummy.next
