bb5b6afcca2e2f100c791a2a403ff071a601bad7
Jenny Li <jli226@fordham.edu>
2020-05-11 14:55:31 -0700

    a24

--- certainTypeQuestions/LinkedList/24.SwapNodesinPairs.py
+++ certainTypeQuestions/LinkedList/24.SwapNodesinPairs.py
@@ -6,3 +6,19 @@
 # Example:
 
 # Given 1->2->3->4, you should return the list as 2->1->4->3.
+class Solution(object):
+        def swapPairs(self, head):
+            if not head or not head.next:
+                return head
+            dummy = ListNode(0)
+            dummy.next = head
+            cur = dummy
+
+            while cur.next and cur.next.next:
+                first = cur.next
+                sec = cur.next.next
+                cur.next = sec
+                first.next = sec.next
+                sec.next = first
+                cur = cur.next.next
+            return dummy.next
