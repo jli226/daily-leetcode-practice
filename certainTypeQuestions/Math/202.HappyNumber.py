121b9486a5ca396dc62bcb4be5d17c209e40f208
Jenny Li <jli226@fordham.edu>
2020-05-12 16:06:05 -0700

    a202

--- certainTypeQuestions/Math/202.HappyNumber.py
+++ certainTypeQuestions/Math/202.HappyNumber.py
@@ -13,3 +13,16 @@
 # 82 + 22 = 68
 # 62 + 82 = 100
 # 12 + 02 + 02 = 1
+
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        memo = set()
+        while n not in memo:
+            memo.add(n)
+            n = sum(int(i)**2 for i in str(n))
+        return 1 in memo
