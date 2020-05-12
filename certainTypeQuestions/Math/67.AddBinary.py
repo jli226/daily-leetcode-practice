f25739cb4224a9199bf5c514bb76d0a7ecad52e3
Jenny Li <jli226@fordham.edu>
2020-05-12 11:34:14 -0700

    a67

--- certainTypeQuestions/Math/67.AddBinary.py
+++ certainTypeQuestions/Math/67.AddBinary.py
@@ -17,3 +17,22 @@
 # Each string consists only of '0' or '1' characters.
 # 1 <= a.length, b.length <= 10^4
 # Each string is either "0" or doesn't contain any leading zero.
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry % 2)
+            carry //= 2
+
+        return result[::-1]
