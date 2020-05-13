40453dcd24aa951bcde1ccafddd2fc2eb079ace3
Jenny Li <jli226@fordham.edu>
2020-05-13 14:39:32 -0700

    a147

--- certainTypeQuestions/Sort/147.InsertionSortList.py
+++ certainTypeQuestions/Sort/147.InsertionSortList.py
@@ -15,3 +15,26 @@
 
 # Input: 4->2->1->3
 # Output: 1->2->3->4
+
+def insertionSortList(self, head):
+
+        dummyHead = ListNode(0)
+        dummyHead.next = nodeToInsert = head
+
+        while head and head.next:
+            if head.val > head.next.val:
+                # Locate nodeToInsert.
+                nodeToInsert = head.next
+                # Locate nodeToInsertPre.
+                nodeToInsertPre = dummyHead
+                while nodeToInsertPre.next.val < nodeToInsert.val:
+                    nodeToInsertPre = nodeToInsertPre.next
+
+                head.next = nodeToInsert.next
+                # Insert nodeToInsert between nodeToInsertPre and nodeToInsertPre.next.
+                nodeToInsert.next = nodeToInsertPre.next
+                nodeToInsertPre.next = nodeToInsert
+            else:
+                head = head.next
+
+        return dummyHead.next
