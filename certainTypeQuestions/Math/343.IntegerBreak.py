45193067c8870165f32539112e335eda3b391255
Jenny Li <jli226@fordham.edu>
2020-05-12 20:31:18 -0700

    a343

--- certainTypeQuestions/Math/343.IntegerBreak.py
+++ certainTypeQuestions/Math/343.IntegerBreak.py
@@ -11,3 +11,22 @@
 # Output: 36
 # Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
 # Note: You may assume that n is not less than 2 and not larger than 58.
+
+
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+        list_3 = [3] * (n/3)  # generate a list of 3
+        mod_3 = n % 3
+        if mod_3 == 1:  # if a 1 is left, then add it to the first element to get a 4
+            list_3[0] += 1
+        if mod_3 == 2:  # if a 2 is left, then put it into the list
+            list_3.append(2)
+        return reduce(lambda a, b: a*b, list_3)
