1e1aa96cbc86411c8aa48cb17fa1b6bbd371374d
Jenny Li <jli226@fordham.edu>
2020-05-13 10:45:16 -0700

    a242

--- certainTypeQuestions/Sort/242.ValidAnagram.py
+++ certainTypeQuestions/Sort/242.ValidAnagram.py
@@ -14,3 +14,11 @@
 # Follow up:
 # What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
+
+def isAnagram1(self, s, t):
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
