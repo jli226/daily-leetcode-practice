171e50f6cb325e39d0ef8faecc56569f108d7ebd
Jenny Li <jli226@fordham.edu>
2020-05-14 11:33:56 -0700

    a63

--- certainTypeQuestions/DynamicProgramming/63.UniquePathsII.py
+++ certainTypeQuestions/DynamicProgramming/63.UniquePathsII.py
@@ -23,3 +23,22 @@
 # There are two ways to reach the bottom-right corner:
 # 1. Right -> Right -> Down -> Down
 # 2. Down -> Down -> Right -> Right
+
+def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        dp = [[0]*len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    if i-1 >= 0:
+                        dp[i][j] += dp[i-1][j]
+                    if j-1 >= 0:
+                        dp[i][j] += dp[i][j-1]
+        return dp[-1][-1]
