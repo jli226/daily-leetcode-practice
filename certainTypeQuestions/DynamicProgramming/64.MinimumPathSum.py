e5541ad8d4b1d9587b92be5a6f087280999c7de1
Jenny Li <jli226@fordham.edu>
2020-05-14 14:22:50 -0700

    a64

--- certainTypeQuestions/DynamicProgramming/64.MinimumPathSum.py
+++ certainTypeQuestions/DynamicProgramming/64.MinimumPathSum.py
@@ -12,3 +12,25 @@
 # ]
 # Output: 7
 # Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if len(grid) <= 0 or grid is None:
+            return 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 and c == 0:  # We just want to skip the top-left corner of the grid
+                    continue
+                if r-1 < 0:  # Cases for elements in top row
+                    grid[r][c] = grid[r][c] + grid[r][c-1]
+                elif c-1 < 0:  # Cases for elements in leftmost column
+                    grid[r][c] = grid[r][c] + grid[r-1][c]
+                else:  # Normal cell
+                    grid[r][c] = grid[r][c] + min(grid[r-1][c], grid[r][c-1])
+
+        # We have got the minimum path accumaled at the bottom-right corner, just return this
+        return grid[rows-1][cols-1]
