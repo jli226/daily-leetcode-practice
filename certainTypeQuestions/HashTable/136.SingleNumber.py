b262805392f91f78f9f35e5fbabf85644502ed79
Jenny Li <jli226@fordham.edu>
2020-05-15 10:57:52 -0700

    a136

--- certainTypeQuestions/HashTable/136.SingleNumber.py
+++ certainTypeQuestions/HashTable/136.SingleNumber.py
@@ -12,3 +12,31 @@
 
 # Input: [4,1,2,1,2]
 # Output: 4
+
+
+def singleNumber1(self, nums):
+    dic = {}
+    for num in nums:
+        dic[num] = dic.get(num, 0)+1
+    for key, val in dic.items():
+        if val == 1:
+            return key
+
+
+def singleNumber2(self, nums):
+    res = 0
+    for num in nums:
+        res ^= num
+    return res
+
+
+def singleNumber3(self, nums):
+    return 2*sum(set(nums))-sum(nums)
+
+
+def singleNumber4(self, nums):
+    return reduce(lambda x, y: x ^ y, nums)
+
+
+def singleNumber(self, nums):
+    return reduce(operator.xor, nums)
