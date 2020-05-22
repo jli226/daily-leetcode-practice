1cb7eeb2d0230e372420141d179d512dbffb0a86
Jenny Li <jli226@fordham.edu>
2020-05-21 22:22:18 -0700

    q&a260

---
+++ certainTypeQuestions/BitManipulation/260.SingleNumberIII.py
@@ -0,0 +1,28 @@
+# Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+# Example:
+
+# Input:  [1,2,1,3,2,5]
+# Output: [3,5]
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        xor = 0
+        a = 0
+        b = 0
+        for num in nums:
+            xor ^= num
+        mask = 1
+        while(xor & mask == 0):
+            mask = mask << 1
+        for num in nums:
+            if num & mask:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
