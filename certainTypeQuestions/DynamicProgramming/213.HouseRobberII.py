86e1c6aacb7d5058c4c750163c45146652ba69f5
Jenny Li <jli226@fordham.edu>
2020-05-29 23:02:38 -0700

    q&a213

---
+++ certainTypeQuestions/DynamicProgramming/213.HouseRobberII.py
@@ -0,0 +1,30 @@
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+
+# Input: [2,3,2]
+# Output: 3
+# Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+#              because they are adjacent houses.
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def simple_rob(nums):
+            rob, not_rob = 0, 0
+            for num in nums:
+                rob, not_rob = not_rob + num, max(rob, not_rob)
+            return max(rob, not_rob)
+
+        if not nums:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+        else:
+            return max(simple_rob(nums[1:]), simple_rob(nums[:-1]))
