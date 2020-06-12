0035273f4d6290bf8db4fe28089eeb7f63185bf9
Jenny Li <jli226@fordham.edu>
2020-06-11 23:06:25 -0700

    q&a392

---
+++ certainTypeQuestions/DynamicProgramming/392.IsSubsequence.py
@@ -0,0 +1,33 @@
+# Given a string s and a string t, check if s is subsequence of t.
+
+# A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+# Follow up:
+# If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+# Credits:
+# Special thanks to @pbrother for adding this problem and creating all test cases.
+
+
+# Example 1:
+
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return True if i == len(s) else False
