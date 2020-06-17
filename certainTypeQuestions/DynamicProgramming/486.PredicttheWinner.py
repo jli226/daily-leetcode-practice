b72465f844ea8714541d52d3c8b8ff1528d61912
Jenny Li <jli226@fordham.edu>
2020-06-16 22:36:58 -0700

    q&a486

---
+++ certainTypeQuestions/DynamicProgramming/486.PredicttheWinner.py
@@ -0,0 +1,29 @@
+# Given an array of scores that are non-negative integers. Player 1 picks one of the numbers from either end of the array followed by the player 2 and then player 1 and so on. Each time a player picks a number, that number will not be available for the next player. This continues until all the scores have been chosen. The player with the maximum score wins.
+
+# Given an array of scores, predict whether player 1 is the winner. You can assume each player plays to maximize his score.
+
+# Example 1:
+# Input: [1, 5, 2]
+# Output: False
+# Explanation: Initially, player 1 can choose between 1 and 2.
+# If he chooses 2 (or 1), then player 2 can choose from 1 (or 2) and 5. If player 2 chooses 5, then player 1 will be left with 1 (or 2).
+# So, final score of player 1 is 1 + 2 = 3, and player 2 is 5.
+# Hence, player 1 will never be the winner and you need to return False.
+
+
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dp = {}
+
+        def find(i, j):
+            if (i, j) not in dp:
+                if i == j:
+                    return nums[i]
+                dp[i, j] = max(nums[i]-find(i+1, j), nums[j]-find(i, j-1))
+            return dp[i, j]
+
+        return find(0, len(nums)-1) >= 0
