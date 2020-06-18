e67816a49d5553466cf80cdf2e578d51d95b6658
Jenny Li <jli226@fordham.edu>
2020-06-17 22:36:17 -0700

    q&a494

---
+++ certainTypeQuestions/DynamicProgramming/494.TargetSum.py
@@ -0,0 +1,31 @@
+# You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+# Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+# Example 1:
+
+# Input: nums is [1, 1, 1, 1, 1], S is 3.
+# Output: 5
+# Explanation:
+
+# -1+1+1+1+1 = 3
+# +1-1+1+1+1 = 3
+# +1+1-1+1+1 = 3
+# +1+1+1-1+1 = 3
+# +1+1+1+1-1 = 3
+
+# There are 5 ways to assign symbols to make the sum of nums be target 3.
+
+
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
