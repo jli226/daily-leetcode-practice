c5d1e7639897da8059746fa725f13523ba4d529a
Jenny Li <jli226@fordham.edu>
2020-06-18 22:47:06 -0700

    q&a516

---
+++ certainTypeQuestions/DynamicProgramming/516.LongestPalindromicSub.py
@@ -0,0 +1,24 @@
+# Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+# Example 1:
+# Input:
+
+# "bbbab"
+# Output:
+# 4
+# One possible longest palindromic subsequence is "bbbb".
+
+
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        d = {}
+
+        def f(s):
+            if s not in d:
+                maxL = 0
+                for c in set(s):
+                    i, j = s.find(c), s.rfind(c)
+                    maxL = max(maxL, 1 if i == j else 2+f(s[i+1:j]))
+                d[s] = maxL
+            return d[s]
+        return f(s)
