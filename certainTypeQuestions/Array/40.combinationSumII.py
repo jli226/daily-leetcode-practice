908bd6553034ffa038a1dcd3ccce333ba97715b7
Jenny Li <jli226@fordham.edu>
2020-07-01 22:01:09 -0700

    a40

--- certainTypeQuestions/Array/40.combinationSumII.py
+++ certainTypeQuestions/Array/40.combinationSumII.py
@@ -16,3 +16,15 @@
 #   [2, 6],
 #   [1, 1, 6]
 # ]
+
+
+def combinationSum2(self, candidates, target):
+    candidates.sort()
+    table = [None] + [set() for i in range(target)]
+    for i in candidates:
+        if i > target:
+            break
+        for j in range(target - i, 0, -1):
+            table[i + j] |= {elt + (i,) for elt in table[j]}
+        table[i].add((i,))
+    return map(list, table[target])
