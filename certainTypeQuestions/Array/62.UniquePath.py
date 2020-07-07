8d0cf89141cb354488ca2226a0b53b16867aff85
Jenny Li <jli226@fordham.edu>
2020-07-06 22:01:44 -0700

    q&a62

---
+++ certainTypeQuestions/Array/62.UniquePath.py
@@ -0,0 +1,15 @@
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+# How many possible unique paths are there?
+
+
+class Solution:
+    # @return an integer
+    def uniquePaths(self, m, n):
+        aux = [[1 for x in range(n)] for x in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                aux[i][j] = aux[i][j-1]+aux[i-1][j]
+        return aux[-1][-1]
