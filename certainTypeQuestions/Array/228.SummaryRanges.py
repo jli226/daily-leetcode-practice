c1966346d303b8e1007aa0731206dc7285d7355b
Jenny Li <jli226@fordham.edu>
2020-08-02 21:27:04 -0700

    a228

--- certainTypeQuestions/Array/228.SummaryRanges.py
+++ certainTypeQuestions/Array/228.SummaryRanges.py
@@ -10,3 +10,13 @@
 # Input:  [0,2,3,4,6,8,9]
 # Output: ["0","2->4","6","8->9"]
 # Explanation: 2,3,4 form a continuous range; 8,9 form a continuous range.
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans = []
+        for i in range(len(nums)): 
+            if i == 0 or nums[i-1] + 1 != nums[i]: stack = [nums[i]]
+            if i == len(nums)-1 or nums[i] + 1 != nums[i+1]: 
+                if stack[-1] != nums[i]: stack.append(nums[i])
+                ans.append(stack)
+        return ["->".join(map(str, x)) for x in ans]
