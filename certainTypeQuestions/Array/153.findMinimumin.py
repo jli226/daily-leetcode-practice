06b89a11c8c746f83278182513d1fd36d57f9744
Jenny Li <jli226@fordham.edu>
2020-08-02 21:18:39 -0700

    a153

--- certainTypeQuestions/Array/153.findMinimumin.py
+++ certainTypeQuestions/Array/153.findMinimumin.py
@@ -14,3 +14,30 @@
 
 # Input: [4,5,6,7,0,1,2]
 # Output: 0
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        # -------------------------------------------
+        
+        def helper(left, right):
+            
+            # base case also known as stop condition
+            if (right - left) <= 1:
+                return min(nums[left], nums[right])
+            
+            
+            # general case
+            mid = left + (right - left) // 2
+            
+            if nums[mid] > nums[right]:
+                # minimum is on the right half
+                return helper(mid+1, right)
+            
+            else:
+                # minimum is on the left half
+                return helper(left, mid)
+            
+        # -------------------------------------------
+        
+        return helper(left=0, right=len(nums)-1)
