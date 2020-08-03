ecf54a3825c8dc441d96fbb17895eeda072c26fb
Jenny Li <jli226@fordham.edu>
2020-08-02 21:24:20 -0700

    a216

--- certainTypeQuestions/Array/216.CombinationSumIII.py
+++ certainTypeQuestions/Array/216.CombinationSumIII.py
@@ -12,3 +12,17 @@
 
 # Input: k = 3, n = 9
 # # Output: [[1,2,6], [1,3,5], [2,3,4]]
+
+        answer = []
+        
+        for num in range(1, 10):
+            self.helper(answer, [num], num, num + 1, k, n)
+        
+        return answer
+    
+    def helper(self, answer, combo, combo_sum, num, k, n):
+        if combo_sum == n and len(combo) == k:
+            answer.append(combo)
+        elif num <= 9 and combo_sum < n and len(combo) < k:
+            self.helper(answer, combo, combo_sum, num + 1, k, n)
+            self.helper(answer, combo + [num], combo_sum + num, num + 1, k, n)
