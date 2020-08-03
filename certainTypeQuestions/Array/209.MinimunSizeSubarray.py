2716217e77506dd79ca2d10f46586c0d85d89dc9
Jenny Li <jli226@fordham.edu>
2020-08-02 21:21:48 -0700

    a209

--- certainTypeQuestions/Array/209.MinimunSizeSubarray.py
+++ certainTypeQuestions/Array/209.MinimunSizeSubarray.py
@@ -7,3 +7,22 @@
 # Explanation: the subarray [4,3] has the minimal length under the problem constraint
 
 # Written with love by atm1504
+# Written with love by atm1504
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        n=len(nums)
+        i=0
+        j=0
+        res=float("inf")
+        t=0
+        while i<n and j<n:
+            t+=nums[j]
+            while t-nums[i]>=s:
+                t-=nums[i]
+                i+=1
+            if t>=s:
+                res=min(res,j-i+1)
+            j+=1
+        if res!=float("inf"):
+            return res
+        return 0
