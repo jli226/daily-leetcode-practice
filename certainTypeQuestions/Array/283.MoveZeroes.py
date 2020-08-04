6273b5c9ef016c24b3caad62f94d709765017ac4
Jenny Li <jli226@fordham.edu>
2020-08-03 22:20:56 -0700

    a283

--- certainTypeQuestions/Array/283.MoveZeroes.py
+++ certainTypeQuestions/Array/283.MoveZeroes.py
@@ -8,3 +8,14 @@
 
 # You must do this in-place without making a copy of the array.
 # Minimize the total number of operations.
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for i in reversed(range(len(nums))):
+            if nums[i] == 0:
+                temp = nums.pop(i)
+                nums.append(temp)
