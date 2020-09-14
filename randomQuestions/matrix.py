9567a7d135184e0e607f541a9ec53366826effc5
Jenny Li <jli226@fordham.edu>
2020-09-14 18:51:10 -0400

    matrix question update

---
+++ randomQuestions/matrix.py
@@ -0,0 +1,9 @@
+# you are working on a logic game made up of a series of puzzles. The first type of puzzle you settle on is "sub-Sudoku", a game where the player has to position the numbers 1...N on an NxN matrix.
+# your job is to write a function that, given an NxN matrix, determines if the matrix is valid. A matrix is valid if every row and column contains exactly the numbers 1...N. For example, in a 4x4 matrix, each row and column would contain the numbers 1, 2, 3, and 4.
+#if the matrix is valid , return "VALID". If it is not Valid, return "INVALID"
+#The matrix may contain any interger, not just 1...N and not just positive. However, the grid will only contain integers.
+
+#example a valid matrix 
+#1 2 3
+#2 3 1
+#3 1 2
